166eec26e73e5e6c22ebf4e1f3db04aba260702e
Stock05 <qace756@gmail.com>
2025-08-02 19:44:28 +0900

    Feat: Weighted Load Balancing

--- gateway/src/main/java/com/sparta/msa_exam/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/sparta/msa_exam/gateway/GatewayApplication.java
@@ -2,10 +2,11 @@ package com.sparta.msa_exam.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import com.sparta.msa_exam.gateway.config.CustomLoadBalancerConfiguration;
 
 @SpringBootApplication
+@LoadBalancerClients(defaultConfiguration = CustomLoadBalancerConfiguration.class)
 public class GatewayApplication {
 
 	public static void main(String[] args) {
--- gateway/src/main/java/com/sparta/msa_exam/gateway/config/CustomLoadBalancerConfiguration.java
+++ gateway/src/main/java/com/sparta/msa_exam/gateway/config/CustomLoadBalancerConfiguration.java
@@ -1,35 +1,18 @@
 package com.sparta.msa_exam.gateway.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
-import java.util.concurrent.ThreadLocalRandom;
-
-@Configuration
-@Slf4j
 public class CustomLoadBalancerConfiguration {
 
     @Bean
-    public ServiceInstanceListSupplier discoveryClientServiceInstanceListSupplier(
-            ConfigurableApplicationContext context) {
-        return ServiceInstanceListSupplier.builder()
-                .withDiscoveryClient()
-                .withWeighted(this::setWeight)
-                .build(context);
-    }
-
-    public int setWeight(ServiceInstance instance){
-        int port = instance.getPort();
-        log.info("port is {}", port);
-        if (port == 19093) {
-            return 70; // 가중치 70
-        } else if (port == 19094) {
-            return 30; // 가중치 30
-        }
-        return 1; // 기본 가중치
+    public ReactorLoadBalancer<ServiceInstance> weightedLoadBalancer(
+            Environment environment, LoadBalancerClientFactory loadBalancerClientFactory) {
+        String serviceId = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new WeightedLoadBalancer(loadBalancerClientFactory.getLazyProvider(serviceId, ServiceInstanceListSupplier.class), serviceId);
     }
 }
