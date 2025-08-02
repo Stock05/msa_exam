4964b2604118c5254056789575f813544053b1b7
Stock05 <qace756@gmail.com>
2025-08-03 02:08:29 +0900

    Feat: 서버포트 헤더

---
+++ order/src/main/java/com/sparta/msa_exam/order/filter/ServerPortFilter.java
@@ -0,0 +1,19 @@
+package com.sparta.msa_exam.order.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class ServerPortFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.addHeader("server-port", String.valueOf(request.getServerPort()));
+        chain.doFilter(request, response);
+    }
+}
